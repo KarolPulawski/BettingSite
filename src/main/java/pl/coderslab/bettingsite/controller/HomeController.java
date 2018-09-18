ed6d09a58533db4ef6269450276f2b330cb2da71
KarolPulawski <karol.j.pulawski@gmail.com>
2018-09-18 22:16:36 +0200

    BettingSite / Controller actions were changed from get to post.

--- src/main/java/pl/coderslab/bettingsite/controller/HomeController.java
+++ src/main/java/pl/coderslab/bettingsite/controller/HomeController.java
@@ -61,128 +61,128 @@ public class HomeController {
 
     }
 
-    @RequestMapping("/get-events")
-    public String getScheduledEvents(Model model) throws ServletException, IOException {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String url = "http://localhost:8080/home/gameWeekSchedule";
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<GameDto[]> responseGames = restTemplate.getForEntity(
-                url, GameDto[].class);
-        GameDto[] games = responseGames.getBody();
-        for (GameDto game : games) {
-//            logger.info("countries {}", country);
-            System.out.println(game.getGameId() + ") " + game.getTeamHome() + " : " + game.getTeamAway() + " | "
-                    + game.getStarted() + " | " + game.getHomeOdd() + " - " + game.getDrawOdd() + " - "
-                    + game.getAwayOdd());
-        }
-        System.out.println("**** size games: " + games.length + "******");
-        model.addAttribute("games", games);
+//    @RequestMapping("/get-events")
+//    public String getScheduledEvents(Model model) throws ServletException, IOException {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        String url = "http://localhost:8080/home/gameWeekSchedule";
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<GameDto[]> responseGames = restTemplate.getForEntity(
+//                url, GameDto[].class);
+//        GameDto[] games = responseGames.getBody();
+//        for (GameDto game : games) {
+////            logger.info("countries {}", country);
+//            System.out.println(game.getGameId() + ") " + game.getTeamHome() + " : " + game.getTeamAway() + " | "
+//                    + game.getStarted() + " | " + game.getHomeOdd() + " - " + game.getDrawOdd() + " - "
+//                    + game.getAwayOdd());
+//        }
+//        System.out.println("**** size games: " + games.length + "******");
+//        model.addAttribute("games", games);
+//
+//        return "game_display";
+//    }
 
-        return "game_display";
-    }
+//    @RequestMapping("/get-results")
+//    public String getResults(Model model) throws ServletException, IOException {
+//        String url = "http://localhost:8080/home/gameWeekResults";
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<GameResultDto[]> responseGames = restTemplate.getForEntity(
+//                url, GameResultDto[].class);
+//        GameResultDto[] games = responseGames.getBody();
+//        for (GameResultDto game : games) {
+////            logger.info("countries {}", country);
+//            System.out.println(game.getGameId() + ") " + game.getTeamHome() + " " + game.getHomeGoal() + ":"
+//                    + game.getAwayGoal() + " " + game.getTeamAway() + " | "
+//                    + game.getStarted() + " | " + game.getHomeOdd() + " - " + game.getDrawOdd() + " - "
+//                    + game.getAwayOdd()
+//                    + " yellow: " + game.getHomeYellow() + "|" + game.getAwayYellow());
+//        }
+//        System.out.println("**** size games: " + games.length + "******");
+//        model.addAttribute("games", games);
+//        return "game_results_display";
+//    }
 
-    @RequestMapping("/get-results")
-    public String getResults(Model model) throws ServletException, IOException {
-        String url = "http://localhost:8080/home/gameWeekResults";
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<GameResultDto[]> responseGames = restTemplate.getForEntity(
-                url, GameResultDto[].class);
-        GameResultDto[] games = responseGames.getBody();
-        for (GameResultDto game : games) {
-//            logger.info("countries {}", country);
-            System.out.println(game.getGameId() + ") " + game.getTeamHome() + " " + game.getHomeGoal() + ":"
-                    + game.getAwayGoal() + " " + game.getTeamAway() + " | "
-                    + game.getStarted() + " | " + game.getHomeOdd() + " - " + game.getDrawOdd() + " - "
-                    + game.getAwayOdd()
-                    + " yellow: " + game.getHomeYellow() + "|" + game.getAwayYellow());
-        }
-        System.out.println("**** size games: " + games.length + "******");
-        model.addAttribute("games", games);
-        return "game_results_display";
-    }
-
-    @PostMapping("/api/game")
-    public String receiveInfoFromApi(@RequestBody GameDto gameDto) {
-
-        Game newGame = new Game();
-
-        Team teamHome = teamServiceImpl.loadTeamByName(gameDto.getTeamHome());
-        Team teamAway = teamServiceImpl.loadTeamByName(gameDto.getTeamAway());
-        newGame.setTeamHome(teamHome);
-        newGame.setTeamAway(teamAway);
-
-        newGame.setActive(gameDto.isActive());
-        newGame.setHistory(gameDto.isHistory());
-        System.out.println("ACTIVE: " + gameDto.isActive());
-
-        Odd odd = new Odd();
-        odd.setAwayOdd(gameDto.getAwayOdd());
-        odd.setDrawOdd(gameDto.getDrawOdd());
-        odd.setHomeOdd(gameDto.getHomeOdd());
-
-        odd = statisticService.generateOdd(newGame);
-
-        oddServiceImpl.addNewOdd(odd);
-        newGame.setOdd(odd);
-
-//        System.out.print("****** scheduled");
-//        System.out.print(newGame.getTeamHome().getName());
-//        System.out.print(" | ");
-//        System.out.print(newGame.getTeamAway().getName());
-//        System.out.print(" | ");
-//        System.out.print(newGame.getActive());
-//        System.out.print(" | ");
-//        System.out.print(newGame.getHistory());
-//        System.out.print("\n");
-        
-        // save to db new games
-        gameServiceImpl.saveGameToDb(newGame);
-        return "test";
-    }
-
-    @PostMapping("/api/result")
-    public String receiveResultApi(@RequestBody GameResultDto gameResultDto) {
-
-        Team teamHome = teamServiceImpl.loadTeamByName(gameResultDto.getTeamHome());
-        System.out.println("name from dto: " + gameResultDto.getTeamHome());
-
-        Game currentGame = gameServiceImpl.findFirstScheduleByTeam(teamHome);
-        System.out.println("name from db: " + currentGame.getTeamHome().getName());
-
-        currentGame.setActive(gameResultDto.isActive());
-        currentGame.setHistory(gameResultDto.isHistory());
-
-        currentGame.setHomeGoal(gameResultDto.getHomeGoal());
-        currentGame.setAwayGoal(gameResultDto.getAwayGoal());
-
-        currentGame.setHomePoint(gameResultDto.getHomePoint());
-        currentGame.setAwayPoint(gameResultDto.getAwayPoint());
-
-        currentGame.setHomeYellow(gameResultDto.getHomeYellow());
-        currentGame.setAwayYellow(gameResultDto.getAwayYellow());
-
-        currentGame.setHomeRed(gameResultDto.getHomeRed());
-        currentGame.setAwayRed(gameResultDto.getAwayRed());
-
-        gameServiceImpl.saveGameToDb(currentGame);
-//        System.out.print("****** result");
-//        System.out.print(game.getTeamHome());
-//        System.out.print(" | ");
-//        System.out.print(game.getHomeGoal());
-//        System.out.print(" | ");
-//        System.out.print(game.getAwayGoal());
-//        System.out.print(" | ");
-//        System.out.print(game.getTeamAway());
-//        System.out.print(" | ");
-//        System.out.print(game.isActive());
-//        System.out.print(" | ");
-//        System.out.print(game.isHistory());
-//        System.out.print(" | ");
-//        System.out.println("\n");
+//    @PostMapping("/api/game")
+//    public String receiveInfoFromApi(@RequestBody GameDto gameDto) {
+//
+//        Game newGame = new Game();
+//
+//        Team teamHome = teamServiceImpl.loadTeamByName(gameDto.getTeamHome());
+//        Team teamAway = teamServiceImpl.loadTeamByName(gameDto.getTeamAway());
+//        newGame.setTeamHome(teamHome);
+//        newGame.setTeamAway(teamAway);
+//
+//        newGame.setActive(gameDto.isActive());
+//        newGame.setHistory(gameDto.isHistory());
+//        System.out.println("ACTIVE: " + gameDto.isActive());
+//
+//        Odd odd = new Odd();
+//        odd.setAwayOdd(gameDto.getAwayOdd());
+//        odd.setDrawOdd(gameDto.getDrawOdd());
+//        odd.setHomeOdd(gameDto.getHomeOdd());
+//
+//        odd = statisticService.generateOdd(newGame);
+//
+//        oddServiceImpl.addNewOdd(odd);
+//        newGame.setOdd(odd);
+//
+////        System.out.print("****** scheduled");
+////        System.out.print(newGame.getTeamHome().getName());
+////        System.out.print(" | ");
+////        System.out.print(newGame.getTeamAway().getName());
+////        System.out.print(" | ");
+////        System.out.print(newGame.getActive());
+////        System.out.print(" | ");
+////        System.out.print(newGame.getHistory());
+////        System.out.print("\n");
+//
 //        // save to db new games
-        return "test";
+//        gameServiceImpl.saveGameToDb(newGame);
+//        return "test";
+//    }
 
-    }
+//    @PostMapping("/api/result")
+//    public String receiveResultApi(@RequestBody GameResultDto gameResultDto) {
+//
+//        Team teamHome = teamServiceImpl.loadTeamByName(gameResultDto.getTeamHome());
+//        System.out.println("name from dto: " + gameResultDto.getTeamHome());
+//
+//        Game currentGame = gameServiceImpl.findFirstScheduleByTeam(teamHome);
+//        System.out.println("name from db: " + currentGame.getTeamHome().getName());
+//
+//        currentGame.setActive(gameResultDto.isActive());
+//        currentGame.setHistory(gameResultDto.isHistory());
+//
+//        currentGame.setHomeGoal(gameResultDto.getHomeGoal());
+//        currentGame.setAwayGoal(gameResultDto.getAwayGoal());
+//
+//        currentGame.setHomePoint(gameResultDto.getHomePoint());
+//        currentGame.setAwayPoint(gameResultDto.getAwayPoint());
+//
+//        currentGame.setHomeYellow(gameResultDto.getHomeYellow());
+//        currentGame.setAwayYellow(gameResultDto.getAwayYellow());
+//
+//        currentGame.setHomeRed(gameResultDto.getHomeRed());
+//        currentGame.setAwayRed(gameResultDto.getAwayRed());
+//
+//        gameServiceImpl.saveGameToDb(currentGame);
+////        System.out.print("****** result");
+////        System.out.print(game.getTeamHome());
+////        System.out.print(" | ");
+////        System.out.print(game.getHomeGoal());
+////        System.out.print(" | ");
+////        System.out.print(game.getAwayGoal());
+////        System.out.print(" | ");
+////        System.out.print(game.getTeamAway());
+////        System.out.print(" | ");
+////        System.out.print(game.isActive());
+////        System.out.print(" | ");
+////        System.out.print(game.isHistory());
+////        System.out.print(" | ");
+////        System.out.println("\n");
+////        // save to db new games
+//        return "test";
+//
+//    }
 
     @GetMapping("/games/scheduled/display")
     public String displayAllActiveGame(Model model) {
@@ -202,7 +202,7 @@ public class HomeController {
         return "game_results_display";
     }
 
-    @GetMapping("/ticket/create")
+    @PostMapping("/ticket/create")
     public String createNewTicket(HttpServletRequest request) {
         HttpSession sess = request.getSession();
         Set<Bet> bets = new HashSet<>();
@@ -251,7 +251,7 @@ public class HomeController {
         return "redirect:/games/scheduled/display";
     }
 
-    @RequestMapping("/ticket/submit")
+    @PostMapping("/ticket/submit")
     public String submitTicket(HttpServletRequest request, Model model) {
         HttpSession sess = request.getSession();
         //download session -> bets
@@ -289,7 +289,7 @@ public class HomeController {
         return "tickets_display";
     }
 
-    @RequestMapping("/ticket/display/{id}")
+    @PostMapping("/ticket/display/{id}")
     public String displayOneTicket(@PathVariable int id, Model model) {
         Ticket ticket = ticketServiceImpl.findTicketById(id);
         model.addAttribute("ticket", ticket);
