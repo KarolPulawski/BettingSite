18556340cc9ae75567ad1ef9373964c98f0a0498
KarolPulawski <karol.j.pulawski@gmail.com>
2018-09-18 13:53:33 +0200

    BettingSite / CSRF was disabled.

--- src/main/java/pl/coderslab/bettingsite/SecurityConfig.java
+++ src/main/java/pl/coderslab/bettingsite/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
                 .antMatchers("/login").permitAll()
